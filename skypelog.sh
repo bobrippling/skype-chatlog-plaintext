b884cf3766daa87d81381d68df023b4cf3110360
muffato <muffato@ebi.ac.uk>
2012-07-01 18:05:42 +0100

    Does not print the path to the iconv binary

--- skypelog.sh
+++ skypelog.sh
@@ -10,4 +10,4 @@ dir="$(dirname "$(readlink "$0" 2> /dev/null || echo "$0")")"
 
 # mostly sorted already, but in case the directory order is out...
 
-"$dir"/skypelog "$1" 2> /dev/null | (which iconv && iconv -f utf-8 -t utf-8//ignore || cat  ) | sort
+"$dir"/skypelog "$1" 2> /dev/null | (which iconv > /dev/null && iconv -f utf-8 -t utf-8//ignore || cat  ) | sort
